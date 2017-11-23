af5c173a4e3cec58fa02ebb7ed2c9e11365f0aff
Minesh <mineshmshah@hotmail.co.uk>
2017-11-23 12:11:06 +0000

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { WeatherComponent } from './weather/weather.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
